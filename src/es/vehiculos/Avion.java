4666aa9918d9804a0fd03bacac8cf9fb999fcff4
AntonioGuillenBalmis <antonio.guillen@iesdoctorbalmis.com>
2021-10-18 17:12:59 +0200

    Se ha añadido despegar y aterrizar a avion por Antonio

--- src/es/vehiculos/Avion.java
+++ src/es/vehiculos/Avion.java
@@ -1,9 +1,28 @@
 package es.vehiculos;
 
 public class Avion extends Vehiculo {
+	
+	boolean despegar = false;
+	boolean tocarSuelo = false;
+	boolean aterrizar = false;
+	int velocidad = 100;
+	
+	
 
 	public String volar() {
 		return "volando!!";
+	}
+	
+	public boolean despegar() {
+		if(velocidad==100 && tocarSuelo)
+		despegar=true;
+		return despegar;	
+	}
+	
+	public boolean aterrizar() {
+		if(velocidad<100 && tocarSuelo==true)
+		aterrizar = false;
+		return aterrizar;	
 	}
 	
 }
